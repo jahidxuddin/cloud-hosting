c7cea8e48fb0faab96a062c0ec6c91e6236d2cdf
Jahid Uddin <jahidudd65@gmail.com>
2024-05-07 16:10:55 +0200

    Removed goofy ass frontend & clean up api

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,15 +4,3 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-export async function POST(request): Promise<Response> {
-    const blob = await request.blob();
-    console.log(blob);
-    return new Response();
-
-}
-
-app.post("/fileView", (req, res)=>{
-    return POST(req);
-})
-
