7560070bef95d7bfd3d5a55c1ad80e2f76ffcfcd
Jahid Uddin <jahidudd65@gmail.com>
2024-05-09 19:00:04 +0200

    Update schema & removed goofy ass file

--- website/src/app/controller/[serverId]/settings/page.tsx
+++ website/src/app/controller/[serverId]/settings/page.tsx
@@ -58,7 +58,7 @@ export default function ServerSettings() {
               </SelectContent>
             </Select>
             <div className="flex items-center space-x-2">
-              <Checkbox defaultChecked id="backup" />
+              <Checkbox className="border border-gray-800" defaultChecked id="backup" />
               <label
                 className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                 htmlFor="backup"
